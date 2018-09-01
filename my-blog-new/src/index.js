bbc082e0c38d09340ad99e04d373344fe6f2ff52
Arun Vijayarengan <arunprabu.v@hotmail.com>
2018-09-01 23:02:26 +0530

    updated for day 5 training
    complete app

--- my-blog-new/src/index.js
+++ my-blog-new/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -10,7 +11,7 @@ import { fetchAllPosts } from './actions/index';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 //To Setup Initial Loading
 store.dispatch(fetchAllPosts());
